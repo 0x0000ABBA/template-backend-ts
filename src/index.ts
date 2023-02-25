0672010f100412ed218555798b662159de7b35b8
seregavspb <96838602+seregavspb@users.noreply.github.com>
2023-02-26 00:40:53 +0300

    init: need hot fixes

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,31 @@
-console.log("Hello World");
+import { Router, Redis } from "bot-router";
+import * as config from "../config.json";
+import { Telegraf } from "telegraf";
+
+const TOKEN = config.token;
+const MESSAGES = config.messages;
+const BUTTONS = config.buttons;
+const DOMAIN = config.domain;
+const PORT = config.port;
+
+const bot = new Telegraf(TOKEN);
+
+const startBot = async () => {
+
+    const redis = new Redis("redis://127.0.0.1:6379");
+
+    await redis.init();
+
+    const router = new Router({
+        devMode: true,
+        redis: redis
+    });
+
+    bot.use((ctx, next) => {
+        return next();
+    });
+
+    bot.launch();
+};
+
+startBot();
---
+++ src/types.ts
@@ -0,0 +1,5 @@
+
+export enum Pages {
+    START_PAGE,
+
+};
